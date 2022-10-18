6e2aa8dff5a1b25eee7bf9c1a6c0adbf7ec53412
gyeongman <rudaks012@naver.com>
2022-10-18 23:12:52 +0900

    Feat : 코딩테스트 나이 측정

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -4,38 +4,23 @@ import java.util.*;
 
 // 알고리즘 문제
 class Solution {
-
-    public int[] solution(int denum1, int num1, int denum2, int num2) {
-        int[] answer = new int[2];
-
-        int denum = denum1 * denum2;
-        int num = (num1 * denum2) + (num2 * denum1);
-
-        int gcd = gcd(denum, num);
-        answer[0] = denum / gcd;
-        answer[1] = num / gcd;
+    public int solution(int age) {
+        int answer = 0;
+        int Year = 2022;
+        answer = Year - age + 1;
+        System.out.println("answer = " + answer);
 
         return answer;
     }
-
-    private int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
-    }
 }
-
 //Main 클래스에서 Solution클래스 선언해주기
 public class Main {
 
     public static void main(String[] ars) {
         Solution s = new Solution();
         //입력요소를 선언해줘야 출력값이 나옴
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-        s.solution(a, b, c ,d);
+        int a = 40;
+
+        s.solution(a);
     }
 }
