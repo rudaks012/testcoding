b00b5e761e9263a9f70fbb5506aac60e7bc6d827
안경만 <rudaks012@naver.com>
2022-10-21 14:09:18 +0900

    Feat : 프로그래머스 최빈 값

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,44 +1,47 @@
 package org.programmers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
 // 알고리즘 문제
 class Solution {
 
     public int solution(int[] array) {
         // 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요. 최빈값이 여러 개면 -1을 return 합니다.
         // 최빈값이란, 가장 많이 나타나는 값입니다.
-        List<Integer> list = new ArrayList<>();
-        List<Integer> answer = new ArrayList<>();
-//        int answer = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == array[j]) {
-                    list.add(array[i]);
-//                    answer++;
-                }
-            }
-        }
-        int listSize = list.size();
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (!Objects.equals(list.get(i), list.get(j))) {
-                    list.set(i, -1);
-                    break;
-                }
-            }
-        }
+        // 예를 들어, [1, 3, 4, 3, 3, 2, 1]이 주어졌을 때, 3이 세 번 나타나므로 최빈값은 3입니다.\
+        // 1. 배열의 길이가 1이면 그냥 리턴
+        // 2. 배열의 길이가 2이상이면
+        // 3. 배열의 길이만큼 반복문을 돌면서
+        // 4. 배열의 값이 같으면 카운트를 올린다.
+        // 5. 카운트가 가장 큰 값을 리턴한다.
+        // 6. 카운트가 같으면 -1을 리턴한다.
+        int answer = 0;
+        int max = 0;
+        int count = 0;
+        int[] countArray = new int[array.length];
         if (array.length == 1) {
-            return 1;
+            return array[0];
         }
-        if (list.size() == 0) {
-            return 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    count++;
+                }
+            }
+            countArray[i] = count;
+            count = 0;
         }
-        System.out.println("listSize = " + list.get(0));
-        return list.get(0);
+        for (int i = 0; i < countArray.length; i++) {
+            if (max < countArray[i]) {
+                max = countArray[i];
+                answer = array[i];
+            }
+        }
+        for (int i = 0; i < countArray.length; i++) {
+            if (max == countArray[i] && answer != array[i]) {
+                answer = -1;
+            }
+        }
+        return answer;
+
     }
 }
 
