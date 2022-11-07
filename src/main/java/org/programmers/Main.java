bbdb0a8ec0c164bb7f04cdbdd750dc3392510c14
gyeongman <rudaks012@naver.com>
2022-11-07 21:42:49 +0900

    Feat : 프로그래머스 다음에 올 숫자

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,23 +1,27 @@
 package org.programmers;
 
+import java.util.Arrays;
+
 // 알고리즘 문제
 class Solution {
-    public int solution(String[] babbling) {
-        int answer = 0;
-        String words[] ={"aya", "ye", "woo", "ma"};
+    public int solution(int[] common) {
+//등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+       //등차수열 혹은 등비수열이 아닌 경우는 없습니다.
+        //공비가 0인 경우는 없습니다.
+        //공차가 0인 경우는 없습니다.
 
-        for(String btemp : babbling){
-            for(int i =0; i< 4; i++){
-                if(btemp.indexOf(words[i])!=-1){
-                    btemp=btemp.replace(words[i], ".");
-                }
-            }
-            if(btemp.equals(".")){ answer++; }
-            if(btemp.equals("..")){ answer++; }
-            if(btemp.equals("...")){ answer++; }
-            if(btemp.equals("....")){ answer++; }
+        int check1 = common[0];
+        int check2 = common[1];
+        int[] check = Arrays.copyOf(common, common.length);
+        int[] checkSort = Arrays.stream(check).sorted().toArray();
+        int lastNumber = checkSort[checkSort.length - 1];
+        if (check2 - check1 == 1) {
+            System.out.println("lastNumber = " + lastNumber);
+            return lastNumber + 1;
+        } else {
+            int result = check2 - check1;
+            return lastNumber * result;
         }
-        return answer;
     }
 }
 
@@ -27,8 +31,7 @@ public class Main {
     public static void main(String[] ars) {
         Solution s = new Solution();
         //입력요소를 선언해줘야 출력값이 나옴
-        String[] babbling = {"ata", "yee", "u", "maa", "wyeoo"};
-        s.solution(babbling);
-
+        int[] common = {2, 4, 6, 8, 10};
+        s.solution(common);
     }
 }
