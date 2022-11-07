42d2fc50ca7c098e0010797fe195e1c0075fb897
gyeongman <rudaks012@naver.com>
2022-11-07 21:51:30 +0900

    Feat: 프로그래머스 정답

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,27 +1,15 @@
 package org.programmers;
 
-import java.util.Arrays;
-
-// 알고리즘 문제
 class Solution {
     public int solution(int[] common) {
-//등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
-       //등차수열 혹은 등비수열이 아닌 경우는 없습니다.
-        //공비가 0인 경우는 없습니다.
-        //공차가 0인 경우는 없습니다.
+        int answer = 0;
 
-        int check1 = common[0];
-        int check2 = common[1];
-        int[] check = Arrays.copyOf(common, common.length);
-        int[] checkSort = Arrays.stream(check).sorted().toArray();
-        int lastNumber = checkSort[checkSort.length - 1];
-        if (check2 - check1 == 1) {
-            System.out.println("lastNumber = " + lastNumber);
-            return lastNumber + 1;
+        if((common[1] - common[0]) == (common[2] - common[1])) {
+            answer = common[common.length-1] + (common[1] - common[0]);
         } else {
-            int result = check2 - check1;
-            return lastNumber * result;
+            answer = common[common.length-1] * (common[1] / common[0]);
         }
+        return answer;
     }
 }
 
