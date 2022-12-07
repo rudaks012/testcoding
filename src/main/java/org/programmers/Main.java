e583ac52147cd3dc8c699bcab41421797052b858
gyeongman <rudaks012@naver.com>
2022-12-07 22:09:16 +0900

    Feat : 프로그래머스 등차수열의 합

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,41 +1,22 @@
 package org.programmers;
 
 import java.util.Arrays;
-import java.util.HashMap;
 
 class Solution {
-    public int[] solution(String[] members, String[][] logs) {
-        int[] answer = new int[members.length];
-        HashMap<String, Boolean> memberInfo = new HashMap<>();
-        HashMap<String, Integer> alarmCount = new HashMap<>();
+    public int[] solution(int num, int total) {
 
-        for (String member : members) {
-            memberInfo.put(member, true);
-            alarmCount.put(member, 0);
+        int[] answer = new int[num];
+
+        int middleNumber = total / num;
+        int subtracNumber = total % num == 0 ? num / 2 : num / 2 - 1;
+
+        int startNumber = middleNumber - subtracNumber;
+≠
+        for (int i = 0; i < num; i++) {
+            answer[i] = startNumber + i;
         }
+        System.out.println(Arrays.toString(answer));
 
-        for (String[] log : logs) {
-            String mbrId = log[0];
-            String logTy = log[1];
-
-            if (logTy.equals("ALARM")) {
-                memberInfo.put(mbrId, false);
-            } else if (logTy.equals("ARTICLE")) {
-                for (String member : members) {
-                    if (member.equals(mbrId)) {
-                        continue;
-                    }
-                    if (memberInfo.get(member)) {
-                        alarmCount.put(member, alarmCount.get(member) + 1);
-                    }
-                }
-            }
-
-            for (int i = 0; i < members.length; i++) {
-                answer[i] = alarmCount.get(members[i]);
-            }
-        }
-        System.out.println("answer = " + Arrays.toString(answer));
         return answer;
     }
 }
@@ -46,9 +27,6 @@ public class Main {
     public static void main(String[] ars) {
         Solution s = new Solution();
         //입력요소를 선언해줘야 출력값이 나옴
-        String[] members = {"A", "B", "C"};
-        //logs [[“A”, “ARTICLE”], [“A”, “ALARM”], [“B”, “ARTICLE”], [“C”, “ALARM”], [“B”, “ARTICLE”]]
-        String[][] logs = {{"A", "ARTICLE"}, {"A", "ALARM"}, {"B", "ARTICLE"}, {"C", "ALARM"}, {"B", "ARTICLE"}};
-        s.solution(members, logs);
+        s.solution(3, 12);
     }
 }
