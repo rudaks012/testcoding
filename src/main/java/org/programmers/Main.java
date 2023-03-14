10d637f09d64d7c67615f890b5b9e9107b7cab23
gyeongman <rudaks012@naver.com>
2023-03-14 21:44:13 +0900

    Feat : 짝수의 값 더하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,13 +1,16 @@
 package org.programmers;
 
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
 
-    public int solution(int num1, int num2) {
+    public int solution(int n) {
 
-        return num1 % num2;
-    }}
+        return IntStream.rangeClosed(0, n)
+                        .filter(i -> i % 2 == 0)
+                        .sum();
+    }
+}
 
 //Main 클래스에서 Solution클래스 선언해주기
 public class Main {
@@ -15,6 +18,6 @@ public class Main {
     public static void main(String[] ars) {
         Solution s = new Solution();
         //입력요소를 선언해줘야 출력값이 나옴
-        s.solution(3, 2);
+        s.solution(10);
     }
 }
