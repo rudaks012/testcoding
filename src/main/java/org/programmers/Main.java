b1a8b14223c93b135a490179407ed1e610baff6a
gyeongman <rudaks012@naver.com>
2023-03-14 22:01:44 +0900

    Feat :배열 자르기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,14 +1,17 @@
 package org.programmers;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 class Solution {
 
-    public int solution(int n) {
-
-        return IntStream.rangeClosed(0, n)
-                        .filter(i -> i % 2 == 0)
-                        .sum();
+    public int[] solution(int[] numbers, int num1, int num2) {
+        //return Arrays.stream(numbers).skip(num1).limit(num2 - num1 + 1).toArray();
+        System.out.println(
+            Arrays.toString(Arrays.stream(numbers)
+                                  .skip(num1)
+                                  .limit(num2 - num1 + 1)
+                                  .toArray()));
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }
 
@@ -18,6 +21,6 @@ public class Main {
     public static void main(String[] ars) {
         Solution s = new Solution();
         //입력요소를 선언해줘야 출력값이 나옴
-        s.solution(10);
+        s.solution(new int[]{1, 2, 3, 4, 5}, 1, 3);
     }
 }
