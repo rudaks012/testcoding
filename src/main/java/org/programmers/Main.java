3807ece67372b92be0f947b53820465dbe23a4f5
gyeongman <rudaks012@naver.com>
2023-03-15 22:00:11 +0900

    Feat :배열 자르기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,17 +1,32 @@
 package org.programmers;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Solution {
 
-    public int[] solution(int[] numbers, int num1, int num2) {
-        //return Arrays.stream(numbers).skip(num1).limit(num2 - num1 + 1).toArray();
-        System.out.println(
-            Arrays.toString(Arrays.stream(numbers)
-                                  .skip(num1)
-                                  .limit(num2 - num1 + 1)
-                                  .toArray()));
-        return Arrays.copyOfRange(numbers, num1, num2 + 1);
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        IntStream stream = Arrays.stream(num_list);
+        Map<Boolean, Long> counts = stream.boxed()
+                                          .collect(
+                                              Collectors.partitioningBy(
+                                                  n -> n % 2 == 0,
+                                                  Collectors.counting()
+                                              )
+                                          );
+        long even = counts.getOrDefault(true, 0L);
+        long odd = counts.getOrDefault(false, 0L);
+        answer[0] = (int) even;
+        answer[1] = (int) odd;
+
+//            int even = (int) Arrays.stream(num_list).filter(n -> n % 2 == 0).count();
+//            int odd = num_list.length - even;
+//            return new int[]{even, odd};
+
+        return answer;
     }
 }
 
@@ -21,6 +36,6 @@ public class Main {
     public static void main(String[] ars) {
         Solution s = new Solution();
         //입력요소를 선언해줘야 출력값이 나옴
-        s.solution(new int[]{1, 2, 3, 4, 5}, 1, 3);
+        s.solution(new int[]{1, 3, 5, 7});
     }
 }
