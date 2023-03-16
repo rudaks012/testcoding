89e9391a5ff1be391749ab42685293b7a1d50686
gyeongman <rudaks012@naver.com>
2023-03-16 20:52:23 +0900

    Feat: 머쓱이 보다 키큰사람

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,34 +1,17 @@
 package org.programmers;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 class Solution {
-
-    public int[] solution(int[] num_list) {
-        int[] answer = new int[2];
-        IntStream stream = Arrays.stream(num_list);
-        Map<Boolean, Long> counts = stream.boxed()
-                                          .collect(
-                                              Collectors.partitioningBy(
-                                                  n -> n % 2 == 0,
-                                                  Collectors.counting()
-                                              )
-                                          );
-        long even = counts.getOrDefault(true, 0L);
-        long odd = counts.getOrDefault(false, 0L);
-        answer[0] = (int) even;
-        answer[1] = (int) odd;
-
-//            int even = (int) Arrays.stream(num_list).filter(n -> n % 2 == 0).count();
-//            int odd = num_list.length - even;
-//            return new int[]{even, odd};
-
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        for (int i : array) {
+            if (i > height) {
+                answer += 1;
+            }
+        }
         return answer;
     }
 }
+
 
 //Main 클래스에서 Solution클래스 선언해주기
 public class Main {
@@ -36,6 +19,6 @@ public class Main {
     public static void main(String[] ars) {
         Solution s = new Solution();
         //입력요소를 선언해줘야 출력값이 나옴
-        s.solution(new int[]{1, 3, 5, 7});
+        s.solution(new int[]{149,180,192,170}, 167);
     }
 }
