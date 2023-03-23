093abb828560603c2d957da1bab251907f1ff62e
gyeongman <rudaks012@naver.com>
2023-03-23 21:59:59 +0900

    Feat : 최대값 두수 곱하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -4,10 +4,9 @@ import java.util.Arrays;
 
 class Solution {
 
-    public int solution(int[] dot) {
-        return (dot[0] > 0)
-            ? (dot[1] > 0 ? 1 : 4)
-            : (dot[1] > 0 ? 2 : 3);
+    public int solution(int[] numbers) {
+        int[] sort = Arrays.stream(numbers).sorted().toArray();
+        return sort[sort.length-1] * sort[sort.length-2];
     }
 }
 
@@ -17,6 +16,6 @@ public class Main {
     public static void main(String[] ars) {
         Solution s = new Solution();
         //입력요소를 선언해줘야 출력값이 나옴
-        s.solution(new int[]{2, 4});
+        s.solution(new int[]{1, 2, 3, 4, 5});
     }
 }
