a685db9e1a0da8f21c7514916083691c0b11aa32
gyeongman <rudaks012@naver.com>
2023-03-23 23:09:58 +0900

    Feat: 배열 원소의 길이

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,11 +1,13 @@
 package org.programmers;
 
+import java.util.Arrays;
+
 class Solution {
 
-    public int solution(int n) {
-        int answer = 0;
-        answer = (n + 6) / 7;
-        return answer;
+    public int[] solution(String[] strlist) {
+        return Arrays.stream(strlist)
+                     .mapToInt(String::length)
+                     .toArray();
     }
 }
 
@@ -15,7 +17,7 @@ public class Main {
     public static void main(String[] ars) {
         Solution s = new Solution();
         //입력요소를 선언해줘야 출력값이 나옴
-        int n = 15;
-        s.solution(n);
+
+        s.solution(new String[]{"We", "are", "the", "world!"});
     }
 }
