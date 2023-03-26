aab0bd8a581df1b5f004fe8ec5a78511f6816f8e
gyeongman <rudaks012@naver.com>
2023-03-26 23:35:16 +0900

    Feat: 2차원 배열 만들기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -6,16 +6,11 @@ import java.util.stream.IntStream;
 class Solution {
 
     public int[][] solution(int[] num_list, int n) {
-        int rows = (int) Math.ceil((double) num_list.length / n);
+        int row = (int) Math.ceil((double) num_list.length / n);
 
-        //배열을 2차원 배열로 만들어주는 함수
-        //IntStream.range(0, rows) : 0부터 rows까지의 숫자를 만들어줌
-        //mapToObj(i -> Arrays.copyOfRange(num_list, i * n, Math.min((i + 1) * n, num_list.length))) : num_list를 i * n부터 (i + 1) * n까지의 배열로 만들어줌
-        //Math.min((i + 1) * n, num_list.length) : (i + 1) * n이 num_list.length보다 크면 num_list.length를 반환
-        //toArray(int[][]::new) : 2차원 배열로 만들어줌
-        return IntStream.range(0, rows)
-                        .mapToObj(i -> Arrays.copyOfRange(num_list, i * n, Math.min((i + 1) * n, num_list.length)))
-                        .toArray(int[][]::new);
+        return IntStream.range(0, row)
+            .mapToObj(i ->Arrays.copyOfRange(num_list, i * n, Math.min((i + 1) * n, num_list.length)))
+            .toArray(int[][]::new);
     }
 }
 
