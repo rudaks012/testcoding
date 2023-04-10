a9828eb6fc47cef136de0dc6b5c18970fa71e534
gyeongman <rudaks012@naver.com>
2023-04-10 22:59:33 +0900

    Feat : 문자 반복 출력하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,73 +1,22 @@
 package org.programmers;
-import java.util.ArrayList;
+
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 class Solution {
-    public int[] solution(int n, int[] x1, int[] y1, int[] x2, int[] y2) {
+    public String solution(String my_string, int n) {
+        StringBuilder result = new StringBuilder();
 
-        List<List<Integer>> listAdjust = IntStream.range(0, n)
-                                                  .<List<Integer>>mapToObj (i -> new ArrayList<>())
-                                                  .collect(Collectors.toList());
-
-        IntStream.range(0, n).forEach(i -> {
-            IntStream.range(i + 1, n).forEach(j -> {
-                if(shareBorder(x1[i], y1[i], x2[i], y2[i], x1[j], y1[j], x2[j], y2[j])) {
-                    listAdjust.get(i).add(j);
-                    listAdjust.get(j).add(i);
-                }
-            });
-        });
-
-        int[] answer = new int[n];
-        for(int i = 0; i < n; i++) {
-            boolean[] visited = new boolean[n];
-            answer[i] = dfs(i, listAdjust, visited);
+        for (int i = 0; i < my_string.length(); i++) {
+            result.append(String.valueOf(my_string.charAt(i)).repeat(Math.max(0, n)));
         }
 
-        return answer;
-
-    }
-    private boolean shareBorder(int x1a, int y1a, int x2a, int y2a, int x1b, int y1b, int x2b, int y2b) {
-        return isShareBorder(x1a, y1a, x2a, y2a, x1b, y1b, x2b, y2b);
-    }
-
-    private boolean isShareBorder(int x1a, int y1a, int x2a, int y2a, int x1b, int y1b, int x2b, int y2b) {
-        return (x1a == x2b || x2a == x1b || y1a == y2b || y2a == y1b) &&
-            (Math.max(x1a, x1b) < Math.min(x2a, x2b) || Math.max(y1a, y1b) < Math.min(y2a, y2b));
-
-    }
-
-    // dfs
-    //깊이우선탐색 알고리즘
-
-    private int dfs(int node, List<List<Integer>> listAdjust, boolean[] visited) {
-
-        if(visited[node]) {
-            return 0;
-        }
-
-        visited[node] = true;
-
-        int count = 1;
-
-        count += listAdjust.get(node)
-                           .stream()
-                           .mapToInt(neighbor -> neighbor)
-                           .map(neighbor -> dfs(neighbor, listAdjust, visited))
-                           .sum();
-
-        return count;
+        return result.toString();
     }
 }
-
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] answer = s.solution(4, new int[]{10, 30, 65, 10}, new int[]{15, 40, 40, 70}, new int[]{40, 65, 80, 30}, new int[]{40, 60, 70, 90});
-        System.out.println(Arrays.toString(answer));
+        s.solution("hello",3);
     }
 }
