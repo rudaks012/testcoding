0bc9e935621d36e51db5a10d88cea0e4c2021652
gyeongman <rudaks012@naver.com>
2023-04-15 00:19:00 +0900

    Feat :문자열 밀기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,12 +3,16 @@ package org.programmers;
 import java.util.stream.IntStream;
 
 class Solution {
+    public int solution(String A, String B) {
 
-    public int[] solution(int n) {
-
-        return IntStream.rangeClosed(1, n)
-                        .filter(i -> i % 2 == 1)
-                        .toArray();
+        int n = A.length();
+        for (int i = 0; i < n; i++) {
+            if (A.equals(B)) { // A와 B가 같으면 현재 이동 횟수 반환
+                return i;
+            }
+            A = A.charAt(n - 1) + A.substring(0, n - 1); // A의 문자를 오른쪽으로 한 칸씩 밀기
+        }
+        return -1;
     }
 }
 
@@ -16,6 +20,6 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.solution(10);
+        s.solution("hello", "ohell");
     }
 }
