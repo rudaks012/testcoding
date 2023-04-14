4d601796ca90c63fdaf6a70ba4665c7d30d1234b
gyeongman <rudaks012@naver.com>
2023-04-15 00:23:17 +0900

    Feat :문자열 밀기 공부

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -5,14 +5,9 @@ import java.util.stream.IntStream;
 class Solution {
     public int solution(String A, String B) {
 
-        int n = A.length();
-        for (int i = 0; i < n; i++) {
-            if (A.equals(B)) { // A와 B가 같으면 현재 이동 횟수 반환
-                return i;
-            }
-            A = A.charAt(n - 1) + A.substring(0, n - 1); // A의 문자를 오른쪽으로 한 칸씩 밀기
-        }
-        return -1;
+        String tempB = B.repeat(3);
+
+        return tempB.indexOf(A);
     }
 }
 
