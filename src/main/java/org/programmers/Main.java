68dd0984e66233286bc657ec53682312da08cb8e
gyeongman <rudaks012@naver.com>
2023-04-17 13:56:38 +0900

    Feat : 배열의 유사도

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,9 +1,13 @@
 package org.programmers;
 
+import java.util.Arrays;
+
 class Solution {
-    public int solution(String message) {
-        int answer = message.length() * 2;
-        return answer;
+
+    public int solution(String[] s1, String[] s2) {
+        return (int) Arrays.stream(s1)
+                           .flatMap(s -> Arrays.stream(s2).filter(s::equals))
+                           .count();
     }
 }
 
@@ -11,6 +15,6 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.solution("bus");
+        s.solution(new String[]{"a", "b", "c"}, new String[]{"com", "b", "d", "p", "c"});
     }
 }
