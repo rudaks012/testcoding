3f0384d303c47b713fd05ebcb14f2b25c61ee01d
gyeongman <rudaks012@naver.com>
2023-04-18 20:18:40 +0900

    Feat : 자릿수 더하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 
 class Solution {
 
-    public int solution(String[] s1, String[] s2) {
-        return (int) Arrays.stream(s1)
-                           .flatMap(s -> Arrays.stream(s2).filter(s::equals))
-                           .count();
+    public int solution(int n) {
+        String number = String.valueOf(n);
+
+        return Arrays.stream(number.split(""))
+                     .mapToInt(Integer::parseInt)
+                     .sum();
     }
 }
 
@@ -15,6 +17,6 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.solution(new String[]{"a", "b", "c"}, new String[]{"com", "b", "d", "p", "c"});
+        s.solution(1234);
     }
 }
