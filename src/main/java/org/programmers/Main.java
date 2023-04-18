7c461586c0d3496e87c8dba43078e7ef5ad4e45c
gyeongman <rudaks012@naver.com>
2023-04-18 22:50:38 +0900

    Feat : 대문자와 소문자 스트림

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,23 +1,20 @@
 package org.programmers;
 
 
-class Solution {
-    public String solution(String my_string) {
-        StringBuilder answer = new StringBuilder();
-        char[] charArray = my_string.toCharArray();
-        for (char c : charArray) {
-            if (Character.isUpperCase(c)) {
-                String data = String.valueOf(c).toLowerCase();
-                answer.append(data);
-            } else {
-                String data = String.valueOf(c).toUpperCase();
-                answer.append(data);
-            }
-        }
+import static java.lang.Character.*;
 
-        return answer.toString();
+import java.util.stream.Collectors;
+
+class Solution {
+
+    public String solution(String my_string) {
+
+        return my_string.chars()
+                        .mapToObj(operand -> String.valueOf((char) (Character.isLowerCase(operand) ? Character.toUpperCase(operand): Character.toLowerCase(operand))))
+                        .collect(Collectors.joining());
     }
 }
+
 
 public class Main {
 
