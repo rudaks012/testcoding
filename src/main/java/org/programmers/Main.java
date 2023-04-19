9f3afe202f0876c010476762f5167a65aad601f2
gyeongman <rudaks012@naver.com>
2023-04-19 21:43:37 +0900

    Feat : 가위바위보

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -7,6 +7,7 @@ class Solution {
 
     public String solution(String rsp) {
 
+        //chars == IntStream -> char로 바꾼후 데이터 취함
         return rsp.chars()
                   .mapToObj(c -> c == '2' ? '0' : (c == '0' ? '5' : '2'))
                   .map(String::valueOf)
