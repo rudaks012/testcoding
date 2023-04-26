4ee7bdf9d922b1ff398d3f94ebc8f95a5748ff24
gyeongman <rudaks012@naver.com>
2023-04-26 22:38:51 +0900

    Feat : 최대값 만들기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,9 +3,17 @@ package org.programmers;
 import java.util.Arrays;
 
 class Solution {
-
     public int solution(int[] numbers) {
         int answer = 0;
+        Arrays.sort(numbers); // 배열을 오름차순 정렬합니다.
+        int n = numbers.length; // 배열의 길이를 저장합니다.
+
+        // 최댓값은 가장 큰 두 양수의 곱이거나 가장 작은 두 음수의 곱일 수 있습니다.
+        int maxProductPositive = numbers[n-1] * numbers[n-2];
+        int maxProductNegative = numbers[0] * numbers[1];
+
+        answer = Math.max(maxProductPositive, maxProductNegative); // 두 경우 중 최댓값을 선택합니다.
+
         return answer;
     }
 }
