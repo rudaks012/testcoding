782673e1aa23b8a517bfdcef845559f5339bae7e
gyeongman <rudaks012@naver.com>
2023-05-01 22:50:01 +0900

    Feat : 문자열 정렬하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,29 +3,20 @@ package org.programmers;
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] numbers) {
-        int answer = 0;
-        Arrays.sort(numbers);
-        int max = numbers[numbers.length - 1];
-        int[] arr = new int[max + 1];
-        for (int i = 0; i < numbers.length; i++) {
-            arr[numbers[i]]++;
-        }
-        for (int i = 0; i < arr.length; i++)
-            if (arr[i] == 0) {
-                answer += i;
-            }
+    public int[] solution(String my_string) {
+        String replace = my_string.replaceAll("[^0-9]", "");
+        String[] split = replace.split("");
+        int[] ints = Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(ints);
 
-        return answer;
-
+        return ints;
     }
 }
-
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.solution(new int[]{1, 2, -3, 4, -5});
+        s.solution("hi12392");
     }
 }
