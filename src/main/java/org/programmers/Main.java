fe969dc1d08fadfc18970cf735eafec841c3ef10
gyeongman <rudaks012@naver.com>
2023-05-01 23:20:26 +0900

    Feat : 인덱스 바꾸기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,17 +1,16 @@
 package org.programmers;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
 
     public String solution(String my_string, int num1, int num2) {
-        char[] my_string_array = my_string.toCharArray();
-        char temp = my_string_array[num1];
-        my_string_array[num1] = my_string_array[num2];
-        my_string_array[num2] = temp;
-
-
-        return new String(my_string_array);
+        List<String> list = Arrays.stream(my_string.split("")).collect(Collectors.toList());
+        Collections.swap(list, num1, num2);
+        return String.join("", list);
     }
 }
 
