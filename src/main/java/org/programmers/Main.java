f49e2429209135eb02c59bcece1dc5328e336763
gyeongman <rudaks012@naver.com>
2023-05-01 23:18:27 +0900

    Feat : 인덱스 바꾸기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -4,10 +4,14 @@ import java.util.Arrays;
 
 class Solution {
 
-    public int[] solution(int n, int[] numlist) {
-        return Arrays.stream(numlist)
-            .filter( num -> num % n == 0)
-            .toArray();
+    public String solution(String my_string, int num1, int num2) {
+        char[] my_string_array = my_string.toCharArray();
+        char temp = my_string_array[num1];
+        my_string_array[num1] = my_string_array[num2];
+        my_string_array[num2] = temp;
+
+
+        return new String(my_string_array);
     }
 }
 
@@ -15,6 +19,6 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.solution(3, new int[]{4, 5, 6, 7, 8, 9, 10, 11, 12});
+        s.solution("hello", 1, 2);
     }
 }
