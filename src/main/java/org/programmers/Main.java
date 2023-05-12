e68311bc426b0005bb925e502143f5e9bbb7215b
gyeongman <rudaks012@naver.com>
2023-05-12 23:20:02 +0900

    Feat : 피자 나눠먹기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -4,6 +4,7 @@ import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
 class Solution {
+    // 1. 1부터 n까지의 수 중에서 6의 배수를 찾는다.
     public OptionalInt solution(int n) {
         return IntStream.rangeClosed(1, n)
                 .filter(i -> (i * 6) % n ==0)
