5991c8ae404b72b73079fe62b9eea4fbf855e344
gyeongman <rudaks012@naver.com>
2023-05-13 00:19:14 +0900

    소문자로 바꾸기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,14 +1,8 @@
 package org.programmers;
 
-import java.util.OptionalInt;
-import java.util.stream.IntStream;
-
 class Solution {
-    // 1. 1부터 n까지의 수 중에서 6의 배수를 찾는다.
-    public OptionalInt solution(int n) {
-        return IntStream.rangeClosed(1, n)
-                .filter(i -> (i * 6) % n ==0)
-                .findFirst();
+    public String solution(String myString) {
+        return myString.toLowerCase();
     }
 }
 
@@ -16,6 +10,6 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.solution(6);
+        s.solution("aBcDeFg");
     }
 }
