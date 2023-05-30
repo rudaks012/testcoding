d3544188be18eedf0a9ca3ae5cb780eddd4b084b
안경만 <rudaks012@naver.com>
2023-05-30 17:42:20 +0900

    Feat : BankService add

--- src/main/java/bank/service/BankService.java
+++ src/main/java/bank/service/BankService.java
@@ -2,8 +2,16 @@ package bank.service;
 
 import bank.BankTransaction;
 import bank.account.BankAccount;
+import bank.customer.Customer;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BankService {
+    private Map<String, Customer> customers;
+
+    public BankService() {
+        this.customers = new HashMap<>();
+    }
 
     public void performTransaction(BankAccount account, BankTransaction transaction, double amount) {
         transaction.execute(account, amount);
