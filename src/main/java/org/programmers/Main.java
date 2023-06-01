92c15aaf782c889922cb26bc0a47b28942df2272
안경만 <rudaks012@naver.com>
2023-06-01 17:22:51 +0900

    Feat : 나머지가 1이 되는 수 찾기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,9 +1,13 @@
 package org.programmers;
 
+import java.util.stream.IntStream;
+
 class Solution {
 
-    public String solution(String my_string, int n) {
-        return my_string.substring(0, n);
+    public int solution(int n) {
+
+        return IntStream.rangeClosed(1, n).filter(i -> n % i == 1)
+                        .findFirst().getAsInt();
     }
 }
 
@@ -11,6 +15,6 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.solution("ProgrammerS123", 11);
+        s.solution(10);
     }
 }
