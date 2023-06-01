4e857723f98a687bf477c45d4bc82a27465a2837
안경만 <rudaks012@naver.com>
2023-06-01 16:28:29 +0900

    Feat : 문자열 앞의 n글자

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,8 +3,7 @@ package org.programmers;
 class Solution {
 
     public String solution(String my_string, int n) {
-        int length = my_string.length();
-        return my_string.substring(length - n, length);
+        return my_string.substring(0, n);
     }
 }
 
