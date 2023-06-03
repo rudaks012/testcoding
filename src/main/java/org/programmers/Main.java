d0ef983363feaa2277afe6436fc639bd8da17dbc
gyeongman <rudaks012@naver.com>
2023-06-03 14:47:06 +0900

    숫자 찾기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,22 +3,25 @@ package org.programmers;
 import java.util.stream.IntStream;
 
 class Solution {
-    public int[] solution(int n) {
-        int sqrt = (int) Math.sqrt(n);
+    public int solution(int num, int k) {
+        String numStr = String.valueOf(num);
 
-        return IntStream.rangeClosed(1, sqrt)
-                .filter(i -> n % i == 0)
-                .flatMap(i -> IntStream.of(i, n / i))
-                .distinct()
-                .sorted()
-                .toArray();
+
+        return IntStream.range(0, numStr.length())
+                .filter(i -> numStr.charAt(i) - '0' == k)
+                .map(i -> i + 1)
+                .findFirst()
+                .orElse(-1);
+
     }
 }
+
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.solution(24);
+        int solution = s.solution(29183, 1);
+        System.out.println("solution = " + solution);
     }
 }
