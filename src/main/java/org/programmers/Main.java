ab872c0017de53cc1f1390a0701294323d9f3173
안경만 <rudaks012@naver.com>
2023-06-05 15:02:41 +0900

    Feat  : 카카오 신고 결과 받기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,33 +1,51 @@
 package org.programmers;
+
 import java.util.*;
 
 class Solution {
-    public String[] solution(String[] players, String[] callings) {
-        Map<String, Integer> playerMap = new HashMap<>();
-        for (int i = 0; i < players.length; i++) {
-            playerMap.put(players[i], i);
-        }
-        for (String calling : callings) {
-            int index = playerMap.get(calling);
-            if (index > 0) {
-                String temp = players[index - 1];
-                players[index - 1] = calling;
-                players[index] = temp;
-                playerMap.put(calling, index - 1);
-                playerMap.put(temp, index);
+
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int n = id_list.length;
+
+        Map<String, Set<String>> reportedBy = new HashMap<>();
+        Map<String, Integer> reportCount = new HashMap<>();
+
+        for (String r : report) {
+            String[] split = r.split(" ");
+            String reporter = split[0];
+            String reported = split[1];
+
+            if (!reportedBy.containsKey(reporter)) {
+                reportedBy.put(reporter, new HashSet<>());
+            }
+
+            if (reportedBy.get(reporter).add(reported)) {
+                reportCount.put(reported, reportCount.getOrDefault(reported, 0) + 1);
             }
         }
-        return players;
+
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            String reporter = id_list[i];
+            if (reportedBy.containsKey(reporter)) {
+                for (String reported : reportedBy.get(reporter)) {
+                    if (reportCount.get(reported) >= k) {
+                        answer[i]++;
+                    }
+                }
+            }
+        }
+
+        return answer;
     }
 }
-
 
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[] solution = s.solution(new String[]{"mumu", "soe", "poe", "kai", "mine"}, new String[]{"kai", "kai", "mine", "mine"});
+        int[] solution = s.solution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"}, 2);
         System.out.println("solution = " + Arrays.toString(solution));
     }
 }
