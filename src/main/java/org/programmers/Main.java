f279fc1fea521e5661e6f0df2d5fbad88bd5eb37
안경만 <rudaks012@naver.com>
2023-06-08 16:28:56 +0900

    to Stream

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,42 +1,30 @@
 package org.programmers;
-
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-
     public int[] solution(String[] id_list, String[] report, int k) {
-        int n = id_list.length;
-
         Map<String, Set<String>> reportedBy = new HashMap<>();
         Map<String, Integer> reportCount = new HashMap<>();
 
-        for (String r : report) {
+        Arrays.stream(report).forEach(r -> {
             String[] split = r.split(" ");
             String reporter = split[0];
             String reported = split[1];
-
-            if (!reportedBy.containsKey(reporter)) {
-                reportedBy.put(reporter, new HashSet<>());
-            }
-
+            reportedBy.putIfAbsent(reporter, new HashSet<>());
             if (reportedBy.get(reporter).add(reported)) {
                 reportCount.put(reported, reportCount.getOrDefault(reported, 0) + 1);
             }
-        }
+        });
 
-        int[] answer = new int[n];
-        for (int i = 0; i < n; i++) {
-            String reporter = id_list[i];
+        return Arrays.stream(id_list).mapToInt(reporter -> {
             if (reportedBy.containsKey(reporter)) {
-                for (String reported : reportedBy.get(reporter)) {
-                    if (reportCount.get(reported) >= k) {
-                        answer[i]++;
-                    }
-                }
+                return (int) reportedBy.get(reporter).stream()
+                                       .filter(reported -> reportCount.get(reported) >= k)
+                                       .count();
             }
-        }
-
-        return answer;
+            return 0;
+        }).toArray();
     }
 }
 
