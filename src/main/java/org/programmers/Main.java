ff60dc610707b490a82d5c5e818dceec14dcddb0
gyeongman <rudaks012@naver.com>
2023-06-11 14:56:20 +0900

    추억점수

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,30 +1,40 @@
 package org.programmers;
-import java.util.*;
-import java.util.stream.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Solution {
-    public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, Set<String>> reportedBy = new HashMap<>();
-        Map<String, Integer> reportCount = new HashMap<>();
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
 
-        Arrays.stream(report).forEach(r -> {
-            String[] split = r.split(" ");
-            String reporter = split[0];
-            String reported = split[1];
-            reportedBy.putIfAbsent(reporter, new HashSet<>());
-            if (reportedBy.get(reporter).add(reported)) {
-                reportCount.put(reported, reportCount.getOrDefault(reported, 0) + 1);
-            }
-        });
+        Map<String, Integer> scores = IntStream.range(0, name.length)
+                .boxed()
+                .collect(Collectors.toMap(i -> name[i], i -> yearning[i]));
 
-        return Arrays.stream(id_list).mapToInt(reporter -> {
-            if (reportedBy.containsKey(reporter)) {
-                return (int) reportedBy.get(reporter).stream()
-                                       .filter(reported -> reportCount.get(reported) >= k)
-                                       .count();
-            }
-            return 0;
-        }).toArray();
+        return Arrays.stream(photo)
+                .mapToInt(persons -> Arrays.stream(persons)
+                        .filter(scores::containsKey)
+                        .mapToInt(scores::get)
+                        .sum())
+                .toArray();
+
+//        Map<String, Integer> scores = new HashMap<>();
+//        for (int i = 0; i < name.length; i++) {
+//            scores.put(name[i], yearning[i]);
+//        }
+//
+//        int[] answer = new int[photo.length];
+//        for (int i = 0; i < photo.length; i++) {
+//            int sum = 0;
+//            for (String person : photo[i]) {
+//                if (scores.containsKey(person)) {
+//                    sum += scores.get(person);
+//                }
+//            }
+//            answer[i] = sum;
+//        }
+//        return answer;
     }
 }
 
@@ -33,7 +43,9 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] solution = s.solution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"}, 2);
-        System.out.println("solution = " + Arrays.toString(solution));
+        int[] solution = s.solution(new String[]{"may", "kein", "kain", "radi"}, new int[]{5, 10, 1, 3}, new String[][]{{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}});
+        for (int i : solution) {
+            System.out.println(i);
+        }
     }
 }
