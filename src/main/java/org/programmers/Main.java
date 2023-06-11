9937dec96605346c39f1bc7513d14c5473492c18
gyeongman <rudaks012@naver.com>
2023-06-11 17:27:26 +0900

    덧칠하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,29 +3,53 @@ package org.programmers;
 import java.util.*;
 
 class Solution {
-    public int solution(int n, int m, int[] section) {
-        Arrays.sort(section);
+    public int[] solution(String[] keymap, String[] targets) {
+        Map<Character, List<Integer>> keyPresses = new HashMap<>();
 
-        int answer = 0;
-        int i = 0;
-
-        while (i < section.length) {
-            int start = section[i];
-            while (i < section.length && section[i] - start < m) {
-                i++;
+        for (int i = 0; i < keymap.length; i++) {
+            for (int j = 0; j < keymap[i].length(); j++) {
+                char c = keymap[i].charAt(j);
+                if (!keyPresses.containsKey(c)) {
+                    keyPresses.put(c, new ArrayList<>());
+                }
+                keyPresses.get(c).add(i+1);
             }
-            answer++;
         }
-        return answer;
+
+        int[] result = new int[targets.length];
+
+        for (int i = 0; i < targets.length; i++) {
+            String target = targets[i];
+            int lastKeyPress = -1;
+            int totalPresses = 0;
+            for (char c : target.toCharArray()) {
+                if (!keyPresses.containsKey(c)) {
+                    totalPresses = -1;
+                    break;
+                }
+                List<Integer> presses = keyPresses.get(c);
+                int nextPress = presses.get(0);
+                if (nextPress == lastKeyPress && presses.size() > 1) {
+                    nextPress = presses.get(1);
+                }
+                totalPresses += nextPress;
+                lastKeyPress = nextPress;
+            }
+            result[i] = totalPresses;
+        }
+
+        return result;
     }
 }
+
+
 
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int solution = s.solution(8, 4, new int[]{2, 3, 6});
-        System.out.println(solution);
+       int[] solution = s.solution(new String[] {"ABACD", "BCEFD"}, new String[] {"ABCD","AABB"});
+        System.out.println(Arrays.toString(solution));
     }
 }
