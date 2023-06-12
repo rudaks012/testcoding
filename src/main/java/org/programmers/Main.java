b54f77b2c4aa90bfbc4256ccf800f087f393475f
gyeongman <rudaks012@naver.com>
2023-06-12 21:59:17 +0900

    대충 만든 자판

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -4,44 +4,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        Map<Character, List<Integer>> keyPresses = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
 
         for (int i = 0; i < keymap.length; i++) {
             for (int j = 0; j < keymap[i].length(); j++) {
-                char c = keymap[i].charAt(j);
-                if (!keyPresses.containsKey(c)) {
-                    keyPresses.put(c, new ArrayList<>());
-                }
-                keyPresses.get(c).add(i+1);
+                char ch = keymap[i].charAt(j);
+                map.put(ch, Math.min(j + 1, map.getOrDefault(ch, Integer.MAX_VALUE)));
             }
         }
 
         int[] result = new int[targets.length];
+        Arrays.fill(result, -1);
 
         for (int i = 0; i < targets.length; i++) {
-            String target = targets[i];
-            int lastKeyPress = -1;
-            int totalPresses = 0;
-            for (char c : target.toCharArray()) {
-                if (!keyPresses.containsKey(c)) {
-                    totalPresses = -1;
+            int total = 0;
+            for (char ch : targets[i].toCharArray()) {
+                if (map.containsKey(ch)) {
+                    total += map.get(ch);
+                } else {
+                    total = -1;
                     break;
                 }
-                List<Integer> presses = keyPresses.get(c);
-                int nextPress = presses.get(0);
-                if (nextPress == lastKeyPress && presses.size() > 1) {
-                    nextPress = presses.get(1);
-                }
-                totalPresses += nextPress;
-                lastKeyPress = nextPress;
             }
-            result[i] = totalPresses;
+            result[i] = total;
         }
 
         return result;
     }
 }
-
 
 
 
