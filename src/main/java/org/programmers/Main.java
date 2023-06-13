fe9145f6b8507f6199bb8573fedb564966e449f9
안경만 <rudaks012@naver.com>
2023-06-13 15:55:34 +0900

    Feat : 카드 뭉치

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,23 +1,22 @@
 package org.programmers;
 
-import java.util.*;
-import java.util.stream.*;
-
 class Solution {
-    public int[] solution(String[] keymap, String[] targets) {
-        Map<Character, Integer> map = new HashMap<>();
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int index1 = 0, index2 = 0;
+        // 인덱스를 추적하기 위해 index1, index2 변수를 선언하고 0으로 초기화합니다.
 
-        IntStream.range(0, keymap.length)
-                .forEach(i -> IntStream.range(0, keymap[i].length())
-                        .forEach(j -> map.compute(keymap[i].charAt(j),
-                                (ch, old) -> old == null || old > j + 1 ? j + 1 : old)));
+        for (String word : goal) {
+            if (index1 < cards1.length && word.equals(cards1[index1])) {
+                index1++;
+            } else if (index2 < cards2.length && word.equals(cards2[index2])) {
+                index2++;
+            } else {
+                return "No";
+            }
+        }
+        // goal 배열을 순회하면서 cards1, cards2 배열의 단어와 일치하는지 확인합니다.
 
-        return Arrays.stream(targets)
-                .mapToInt(target -> target.chars()
-                        .map(ch -> map.getOrDefault((char) ch, -1))
-                        .filter(value -> value != -1)
-                        .sum())
-                .toArray();
+        return "Yes";
     }
 }
 
@@ -26,7 +25,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] solution = s.solution(new String[]{"ABACD", "BCEFD"}, new String[]{"ABCD", "AABB"});
-        System.out.println(Arrays.toString(solution));
+        String solution = s.solution(new String[]{"i", "drink", "water"}, new String[]{"want", "to"}, new String[]{"i", "want", "to", "drink", "water"});
+        System.out.println(solution);
     }
 }
