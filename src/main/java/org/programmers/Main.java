6e163902dee722987cfd410cebe358f5e33e7f64
gyeongman <rudaks012@naver.com>
2023-06-14 20:39:59 +0900

    둘만의 암호

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,31 +1,31 @@
 package org.programmers;
 
 class Solution {
-    public String solution(String[] cards1, String[] cards2, String[] goal) {
-        int index1 = 0, index2 = 0;
-        // 인덱스를 추적하기 위해 index1, index2 변수를 선언하고 0으로 초기화합니다.
-
-        for (String word : goal) {
-            if (index1 < cards1.length && word.equals(cards1[index1])) {
-                index1++;
-            } else if (index2 < cards2.length && word.equals(cards2[index2])) {
-                index2++;
-            } else {
-                return "No";
+    public String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            for (int i = 0; i < index; ) {
+                ch++;
+                if (ch > 'z') {
+                    ch = 'a';
+                }
+                if (skip.indexOf(ch) == -1) {
+                    i++;
+                }
             }
+            answer.append(ch);
         }
-        // goal 배열을 순회하면서 cards1, cards2 배열의 단어와 일치하는지 확인합니다.
-
-        return "Yes";
+        return answer.toString();
     }
 }
+
 
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        String solution = s.solution(new String[]{"i", "drink", "water"}, new String[]{"want", "to"}, new String[]{"i", "want", "to", "drink", "water"});
+        String solution = s.solution("aukks", "wbqd", 5);
         System.out.println(solution);
     }
 }
