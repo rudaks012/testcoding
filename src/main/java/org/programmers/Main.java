bfe4d865268b91be191696d08c2ef4f4d3b97078
gyeongman <rudaks012@naver.com>
2023-06-14 21:23:00 +0900

    크기가 작은 부분 문자열

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,31 +1,31 @@
 package org.programmers;
 
 class Solution {
-    public String solution(String s, String skip, int index) {
-        StringBuilder answer = new StringBuilder();
-        for (char ch : s.toCharArray()) {
-            for (int i = 0; i < index; ) {
-                ch++;
-                if (ch > 'z') {
-                    ch = 'a';
-                }
-                if (skip.indexOf(ch) == -1) {
-                    i++;
-                }
+    public int solution(String numStr, String targetStr) {
+        int targetLength = targetStr.length();
+        long targetValue = Long.parseLong(targetStr);
+        int occurences = 0;
+
+        // 길이가 targetLength인 창을 numStr에 슬라이드
+        for (int i = 0; i <= numStr.length() - targetLength; i++) {
+            // numStr의 하위 문자열을 가져와서 long으로 변환
+            long subValue = Long.parseLong(numStr.substring(i, i + targetLength));
+
+            // subValue가 targetValue보다 작거나 같으면 카운트를 증가시킵니다.
+            if (subValue <= targetValue) {
+                occurences++;
             }
-            answer.append(ch);
         }
-        return answer.toString();
+        return occurences;
     }
 }
-
 
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        String solution = s.solution("aukks", "wbqd", 5);
+        int solution = s.solution("3141592", "271");
         System.out.println(solution);
     }
 }
