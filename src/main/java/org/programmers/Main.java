6c23bfa4f2fa5fb1d2d25cc00f19597865f59264
gyeongman <rudaks012@naver.com>
2023-06-18 17:43:57 +0900

    가장 가까운 글자

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,33 +1,33 @@
 package org.programmers;
 
+import java.util.*;
+
 class Solution {
-    public int solution(String numStr, String targetStr) {
-        int targetLength = targetStr.length();
-        long targetValue = Long.parseLong(targetStr);
-        int occurences = 0;
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        Map<Character, Integer> charToIndexMap = new HashMap<>();
 
-        // 길이가 targetLength인 창을 numStr에 슬라이드
-        for (int i = 0; i <= numStr.length() - targetLength; i++) {
-            // numStr의 하위 문자열을 가져와서 long으로 변환
-            long subValue = Long.parseLong(numStr.substring(i, i + targetLength));
-
-            // subValue가 targetValue보다 작거나 같으면 카운트를 증가시킵니다.
-            // subValue가 targetValue보다 크면 아무것도하지 않습니다.
-
-            if (subValue <= targetValue) {
-                occurences++;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (charToIndexMap.containsKey(ch)) {
+                answer[i] = i - charToIndexMap.get(ch);
+            } else {
+                answer[i] = -1;
             }
+            charToIndexMap.put(ch, i);
         }
-        return occurences;
+
+        return answer;
     }
 }
+
 
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int solution = s.solution("3141592", "271");
-        System.out.println(solution);
+        int[] solution = s.solution("banana");
+        System.out.println(Arrays.toString(solution));
     }
 }
