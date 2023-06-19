8d12349df370e28906a263403f9957406897ab55
gyeongman <rudaks012@naver.com>
2023-06-19 23:23:45 +0900

    가장 가까운 글자

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -4,6 +4,11 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
+        // 1. 문자열을 순회하면서, 해당 문자가 이전에 나온 적이 있는지 확인한다.
+        // 2. 이전에 나온 적이 있다면, 현재 인덱스에서 이전 인덱스를 뺀 값을 배열에 넣는다.
+        // 3. 이전에 나온 적이 없다면, -1을 배열에 넣는다.
+        // 4. 문자를 순회하면서, 해당 문자의 인덱스를 기록한다.
+        // 5. 배열을 리턴한다.
         int[] answer = new int[s.length()];
         Map<Character, Integer> charToIndexMap = new HashMap<>();
 
