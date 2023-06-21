3822fb3c5bf6021d6f5e815255e1fd21bec4b0a1
gyeongman <rudaks012@naver.com>
2023-06-21 22:45:16 +0900

    기사단원의 무기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,22 +3,39 @@ package org.programmers;
 import java.util.stream.IntStream;
 
 class Solution {
-    public int solution(int[] absolutes, boolean[] signs) {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        for(int i = 1; i <= number; i++) {
+            int count = getDivisorCount(i);
+            answer += count > limit ? power : count;
+        }
+        return answer;
+    }
 
-        return IntStream.range(0, absolutes.length)
-                .map(i -> signs[i] ? absolutes[i] : -absolutes[i])
-                .sum();
+    private int getDivisorCount(int n) {
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                // if divisors are not equal,
+                // add both
+                if (n / i == i) {
+                    count += 1;
+                } else {
+                    count += 2;
+                }
+            }
+        }
+        return count;
     }
 }
+
 
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] absolutes = {4, 7, 12};
-        boolean[] signs = {true, false, true};
-        int solution = s.solution(absolutes, signs);
+        int solution = s.solution(5, 3, 2);
         System.out.println(solution);
     }
 }
