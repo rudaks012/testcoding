bcfbe6cc70680abca205a14618f136236ca95fec
안경만 <rudaks012@naver.com>
2023-06-23 17:51:24 +0900

    Feat : 약수 의 합

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,8 +1,10 @@
 package org.programmers;
 
 class Solution {
-    public String solution(int num) {
-        return num % 2 == 0 ? "Even" : "Odd";
+    public String solution(int num, int num2, int num3) {
+        int sum = num + num2 + num3;
+
+        return sum % 2 == 0 ? "Even" : "Odd";
     }
 }
 
@@ -13,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        String  solution = s.solution(3);
+        String  solution = s.solution(3,5,8);
         System.out.println(solution);
     }
 }
