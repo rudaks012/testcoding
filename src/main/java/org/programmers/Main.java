9b3592aa7f9a7be95a4155fa5475ac3afcce9369
gyeongman <rudaks012@naver.com>
2023-07-01 23:15:26 +0900

    푸드 파이트 대회 알고리즘

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,15 +3,18 @@ package org.programmers;
 class Solution {
     public String solution(int[] food) {
         String answer = "0";
+        // 1. 가장 많이 먹은 음식을 찾는다.
+        // 2. 그 음식을 제외한 나머지 음식을 먹는다.
+        // 3. 1~2번을 반복한다.
+        // 4. 모든 음식을 먹었을 때, 가장 많이 먹은 음식부터 가장 적게 먹은 음식까지 순서대로 정답에 추가한다.
+        // 5. 정답을 반환한다.
+        // 6. 음식을 먹을 수 없는 경우 -1을 반환한다.
+        // 7. 음식을 먹을 수 있는 경우, 정답을 반환한다.
 
-        int i = food.length - 1;
-        while (i > 0) {
-            int j = 0;
-            while (j < food[i] / 2) {
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
                 answer = i + answer + i;
-                j++;
             }
-            i--;
         }
 
         return answer;
