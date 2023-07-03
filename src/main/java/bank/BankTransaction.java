b103c097656e0e22cdd828631452870129682dd4
안경만 <rudaks012@naver.com>
2023-07-03 17:48:02 +0900

    Explan

--- src/main/java/bank/BankTransaction.java
+++ src/main/java/bank/BankTransaction.java
@@ -4,6 +4,10 @@ import bank.account.BankAccount;
 
 public interface BankTransaction {
 
+    //설명
+    //BankTransaction 인터페이스는 execute 메소드를 가지고 있습니다.
+    //execute 메소드는 BankAccount 객체와 double 타입의 amount를 인자로 받습니다.
+
     void execute(BankAccount account, double amount);
 
 }
