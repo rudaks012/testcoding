bd7c80df1879f29e1490f707e8532de17b2be9d9
gyeongman <rudaks012@naver.com>
2023-07-11 22:13:11 +0900

    없는 숫자 더하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,20 +1,17 @@
 package org.programmers;
 
+import java.util.Arrays;
+
 class Solution {
-    public int solution(int[] number) {
-        int answer = 0;
-        // 모든 가능한 세 수의 조합을 찾습니다.
-        for (int i = 0; i < number.length; i++) {
-            for (int j = i + 1; j < number.length; j++) {
-                for (int k = j + 1; k < number.length; k++) {
-                    // 세 수의 합이 0이면, 삼총사를 만들 수 있는 경우의 수를 증가시킵니다.
-                    if (number[i] + number[j] + number[k] == 0) {
-                        answer++;
-                    }
-                }
-            }
+    public int solution(int[] numbers) {
+        int answer = -1;
+        int sum = 45;
+
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length; i++) {
+            sum -= numbers[i];
         }
-        return answer;
+        return sum;
     }
 }
 
@@ -22,7 +19,7 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int solution = s.solution(new int[]{-2, 3, 0, 2, -5});
+        int solution = s.solution(new int[]{1, 2, 3, 4, 6, 7, 8, 0});
         System.out.println(solution);
     }
 }
