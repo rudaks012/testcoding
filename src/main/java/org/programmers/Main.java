57ce782e964d57e6a30fdad8566e63baf5321c53
gyeongman <rudaks012@naver.com>
2023-07-16 23:31:22 +0900

    평균 구하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,18 +3,15 @@ package org.programmers;
 import java.util.stream.IntStream;
 
 class Solution {
-    public int solution(int[] numbers) {
-        return IntStream.rangeClosed(0, 9)
-                .filter(i -> IntStream.of(numbers).noneMatch(j -> j == i))
-                .sum();
+    public double solution(int[] arr) {
+        return IntStream.of(arr).sum() / (double) arr.length;
     }
 }
-
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int solution = s.solution(new int[]{1, 2, 3, 4, 6, 7, 8, 0});
+        double solution = s.solution(new int[]{1, 2, 3, 4});
         System.out.println(solution);
     }
 }
