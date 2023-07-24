f728e0cdaca1ee80eab89482c2805c290b7226f8
안경만 <rudaks012@naver.com>
2023-07-24 16:28:06 +0900

    Feat : 내적

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,17 +3,20 @@ package org.programmers;
 import java.util.stream.IntStream;
 
 class Solution {
-    public double solution(int[] arr) {
-        double v = IntStream.of(arr).sum() / (double) arr.length;
-        // System.out.println("v = " + v);
-        return v;
+
+    public int solution(int[] a, int[] b) {
+        return IntStream.range(0, a.length)
+                        .map(i -> a[i] * b[i])
+                        .sum();
     }
 }
 
+
 public class Main {
+
     public static void main(String[] args) {
         Solution s = new Solution();
-        double solution = s.solution(new int[]{1, 2, 3, 4});
+        double solution = s.solution(new int[]{1, 2, 3, 4}, new int[]{-3, -1, 0, 2});
         System.out.println(solution);
     }
 }
