2bebbf375e2b91765f4c9226a629ac9ff1e5182a
gyeongman <rudaks012@naver.com>
2023-07-27 23:43:48 +0900

    햄버거 만들기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,36 +1,57 @@
 package org.programmers;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Deque;
-import java.util.List;
 
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
-        int[] correctOrder = {1, 2, 3, 1};
-        int[] window = new int[4];
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < ingredient.length; i++) {
-            window[i % 4] = ingredient[i];
-            if (Arrays.equals(window, correctOrder)) {
+        for(int i = 0; i < ingredient.length; i++) {
+            stack.addLast(ingredient[i]);
+
+            while (canMakeBurger(stack)) {
+                makeBurger(stack);
                 answer++;
             }
         }
+
         return answer;
     }
+
+    private boolean canMakeBurger(Deque<Integer> stack) {
+        if (stack.size() < 4) {
+            return false;
+        }
+
+        Integer fourth = stack.pollLast();
+        Integer third = stack.pollLast();
+        Integer second = stack.pollLast();
+        Integer first = stack.pollLast();
+
+        stack.addLast(first);
+        stack.addLast(second);
+        stack.addLast(third);
+        stack.addLast(fourth);
+
+        return first == 1 && second == 2 && third == 3 && fourth == 1; // modified
+    }
+
+    private void makeBurger(Deque<Integer> stack) {
+        stack.pollLast();
+        stack.pollLast();
+        stack.pollLast();
+        stack.pollLast();
+    }
 }
-
-
-
 
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int solution = s.solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1});
+        int solution = s.solution(new int[]{1, 3, 2, 1, 2, 1, 3, 1, 2});
         System.out.println(solution);
     }
 }
