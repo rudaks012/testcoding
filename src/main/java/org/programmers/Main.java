b8df268e2367cfc358e84bb18b0173ff74e8651b
안경만 <rudaks012@naver.com>
2023-07-28 16:42:44 +0900

    Feat : 자열수 뒤집어 배열로 만들기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,57 +1,31 @@
 package org.programmers;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
 
-    public int solution(int[] ingredient) {
-        int[] stack = new int[ingredient.length];
-        int sp = 0;
-        int answer = 0;
-        for (int i : ingredient) {
-            stack[sp++] = i;
-            if (sp >= 4 && stack[sp - 1] == 1 && stack[sp - 2] == 3 && stack[sp - 3] == 2 && stack[sp - 4] == 1) {
-                sp -= 4;
-                answer++;
-            }
+    public int[] solution(long n) {
+        int[] answer = {};
+        String s = String.valueOf(n);
+        String[] split = s.split("");
+        Collections.reverse(Arrays.asList(split));
+
+        answer = new int[split.length];
+        for (int i = 0; i < split.length; i++) {
+            answer[i] = Integer.parseInt(split[i]);
         }
+
+
         return answer;
     }
-
-
-    private boolean canMakeBurger(Deque<Integer> stack) {
-        if (stack.size() < 4) {
-            return false;
-        }
-
-        Integer fourth = stack.pollLast();
-        Integer third = stack.pollLast();
-        Integer second = stack.pollLast();
-        Integer first = stack.pollLast();
-
-        stack.addLast(first);
-        stack.addLast(second);
-        stack.addLast(third);
-        stack.addLast(fourth);
-
-        return first == 1 && second == 2 && third == 3 && fourth == 1; // modified
-    }
-
-    private void makeBurger(Deque<Integer> stack) {
-        stack.pollLast();
-        stack.pollLast();
-        stack.pollLast();
-        stack.pollLast();
-    }
 }
-
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int solution = s.solution(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1});
-        System.out.println(solution);
+        int[] solution = s.solution(12345);
+        System.out.println(Arrays.toString(solution));
     }
 }
