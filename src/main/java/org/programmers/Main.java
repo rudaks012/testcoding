ecf1fdb2d838dcb2d3dfc492b1c51ea09b4b30a0
안경만 <rudaks012@naver.com>
2023-07-28 17:11:41 +0900

    Feat : 자릿수 더하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,16 +1,13 @@
 package org.programmers;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 class Solution {
 
-    public int[] solution(long n) {
-        return new StringBuilder(String.valueOf(n))
-            .reverse()
-            .chars()
-            .map(Character::getNumericValue)
-            .toArray();
+    public int solution(int n) {
+
+        return String.valueOf(n)
+                     .chars()
+                     .map(Character::getNumericValue)
+                     .sum();
     }
 }
 
@@ -18,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] solution = s.solution(12345);
-        System.out.println(Arrays.toString(solution));
+        int solution = s.solution(12345);
+        System.out.println(solution);
     }
 }
