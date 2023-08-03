5b7b7cd05b4edecc82d37f6bc329242b8017706f
gyeongman <rudaks012@naver.com>
2023-08-03 20:03:43 +0900

    Feat : 정수 내림차순으로 배치하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,16 +1,15 @@
 package org.programmers;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        String minus = "";
-        if (s.charAt(0) == '-') {
-            minus = "-";
-            s = s.substring(1);
-        }
-        int parseInt = Integer.parseInt(s);
-        answer = Integer.parseInt(minus + parseInt);
-        return answer;
+    public long solution(long n) {
+        String[] list = String.valueOf(n).split("");
+
+        Arrays.sort(list, Collections.reverseOrder());
+
+        return Long.parseLong(String.join("", list));
     }
 }
 
@@ -18,7 +17,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int solution = s.solution("-1234");
+        long solution = s.solution(118372);
         System.out.println(solution);
     }
 }
