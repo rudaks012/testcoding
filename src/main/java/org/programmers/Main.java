073f3bcae70b10b3ab50484cff2333f1edd2dd26
gyeongman <rudaks012@naver.com>
2023-08-09 23:31:06 +0900

    Feat : 나누어 떨어지는 숫자 배열

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,16 +1,21 @@
 package org.programmers;
 
-import java.util.OptionalInt;
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 class Solution {
-    public String solution(String[] seoul) {
-        OptionalInt index = IntStream.range(0, seoul.length)
-                .filter(i -> seoul[i].equals("Kim"))
-                .findFirst();
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
 
-        return index.isPresent()
-                ? "김서방은 " + index.getAsInt() + "에 있다" : "";
+        for (int j : arr) {
+            if (j % divisor == 0) {
+                answer = Arrays.copyOf(answer, answer.length + 1);
+                answer[answer.length - 1] = j;
+            }
+        }
+        answer = answer.length == 0 ? new int[]{-1} : answer;
+
+        Arrays.sort(answer);
+        return answer;
     }
 }
 
@@ -18,7 +23,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        String solution = s.solution(new String[]{"Jane", "Kim"});
-        System.out.println(solution);
+        int[] solution = s.solution(new int[]{5, 9, 7, 10}, 5);
+        System.out.println(Arrays.toString(solution));
     }
 }
