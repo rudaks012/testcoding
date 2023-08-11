ce289198ebb85cd4f899b0ae6234f861e38d6271
안경만 <rudaks012@naver.com>
2023-08-11 16:43:14 +0900

    Feat : 자릿수 더하기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr, int divisor) {
-        return Arrays.stream(arr).filter(factor -> factor % divisor == 0).toArray();
+        return Arrays.stream(arr)
+                     .filter(factor -> factor % divisor == 0)
+                     .toArray();
     }
 }
 
