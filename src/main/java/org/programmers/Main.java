2e3880568d969d3c461e8d6c100ff73e20ee9100
gyeongman <rudaks012@naver.com>
2023-08-13 23:01:08 +0900

    Feat : 핸드폰 번호 가리기

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -3,10 +3,10 @@ package org.programmers;
 import java.util.Arrays;
 
 class Solution {
-    public int[] solution(int[] arr, int divisor) {
-        return Arrays.stream(arr)
-                     .filter(factor -> factor % divisor == 0)
-                     .toArray();
+    public String solution(String phone_number) {
+        char[] chars = phone_number.toCharArray();
+        Arrays.fill(chars, 0, chars.length - 4, '*');
+        return String.valueOf(chars);
     }
 }
 
@@ -14,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] solution = s.solution(new int[]{5, 9, 7, 10}, 5);
-        System.out.println(Arrays.toString(solution));
+        String solution = s.solution("01033334444");
+        System.out.println(solution);
     }
 }
