a64505ffbb2d3b4e0a93ff9cdfa38587b1c97c68
gyeongman <rudaks012@naver.com>
2023-08-14 23:20:00 +0900

    Feat : 스트림

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,15 +1,17 @@
 package org.programmers;
 
-import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String s) {
-        char[] chars = s.toCharArray();
-        Arrays.sort(chars);
-
-        return new StringBuilder(new String(chars)).reverse().toString();
+        return s.chars()
+                .mapToObj(c -> (char) c)
+                .sorted((a, b) -> b - a)
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
+
 
 public class Main {
 
