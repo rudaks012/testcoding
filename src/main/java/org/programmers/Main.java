fb1c3c2df80431902fd5a23b5102291c8d1ee8a4
gyeongman <rudaks012@naver.com>
2023-08-23 23:57:49 +0900

    Feat : 약수의 개수와 덧셈

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -4,28 +4,41 @@ import java.util.Arrays;
 import java.util.Collections;
 
 class Solution {
-    public int[] solution(int[] arr) {
-        if (arr.length == 1) {
-            return new int[]{-1};
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for (int num = left; num <= right; num++) {
+            int divisorCount = countDivisors(num);
+            if (divisorCount % 2 == 0) {
+                answer += num;
+            } else {
+                answer -= num;
+            }
         }
-        int min = Arrays.stream(arr)
-                .min()
-                .getAsInt();
 
+        return answer;
+    }
 
-        return Arrays.stream(arr)
-                .filter(i -> i != min)
-                .toArray();
+    private int countDivisors(int num) {
+        int count = 0;
+        for (int i = 1; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                count++;
+                if (i != num / i) { // 제곱근 이하에서 나누어 떨어지면, 그 수와 그 수로 나눈 몫 2개의 약수를 카운트
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
-
 
 
 public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] solution = s.solution(new int[]{4, 3, 2, 1});
+        int solution = s.solution(13, 17);
         System.out.println(solution);
     }
 }
