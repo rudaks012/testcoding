f068343756b7b786d85a17f17588d6ef23b33f76
gyeongman <rudaks012@naver.com>
2023-08-24 23:40:02 +0900

    Feat : 문자열 다루기 기분

--- src/main/java/org/programmers/Main.java
+++ src/main/java/org/programmers/Main.java
@@ -1,35 +1,12 @@
 package org.programmers;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 class Solution {
-    public int solution(int left, int right) {
-        int answer = 0;
-
-        for (int num = left; num <= right; num++) {
-            int divisorCount = countDivisors(num);
-            if (divisorCount % 2 == 0) {
-                answer += num;
-            } else {
-                answer -= num;
-            }
+    public boolean solution(String s) {
+        if (s.length() == 4 || s.length() == 6) {
+            return s.matches("^[0-9]*$");
+        } else {
+            return false;
         }
-
-        return answer;
-    }
-
-    private int countDivisors(int num) {
-        int count = 0;
-        for (int i = 1; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                count++;
-                if (i != num / i) { // 제곱근 이하에서 나누어 떨어지면, 그 수와 그 수로 나눈 몫 2개의 약수를 카운트
-                    count++;
-                }
-            }
-        }
-        return count;
     }
 }
 
@@ -38,7 +15,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int solution = s.solution(13, 17);
+        boolean solution = s.solution("a234");
         System.out.println(solution);
     }
 }
